a2dc492fa461349e70f6fc24163aaa087580b705
MartaLB95 <martalaveda@hotmail.com>
2026-01-05 11:37:28 +0100

    Cards completed with images

--- src/components/SectionCard.js
+++ src/components/SectionCard.js
@@ -1,16 +1,24 @@
-export default function SectionCard({title, text, link}) {
+export default function SectionCard({
+  title,
+  text,
+  link,
+  imageSource,
+  imageDescription,
+}) {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img src="..." className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">
-          {text}
-        </p>
-        <a href={link} className="btn btn-primary">
-          Continue reading...
-        </a>
-      </div>
-    </div>
+      <div className="card">
+            <img
+              src={imageSource}
+              className="img-fluid rounded-start"
+              alt={imageDescription}
+            />
+            <div className="card-body">
+              <h5 className="card-title">{title}</h5>
+              <p className="card-text">{text}</p>
+              <a href={link} className="btn btn-primary">
+                Continue reading...
+              </a>
+              </div>
+            </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,16 @@
 import SectionCard from "../components/SectionCard";
+import contactImage from "../img/contact.jpg";
+import subjectImage from "../img/subjects.jpg";
+import methodologyImage from "../img/methodology.jpg";
+import examsImage from "../img/exams.jpg";
 
 export default function Home() {
     return (
-    <div>
-        <SectionCard title="Our Methodology" text="At Screamford Academy, we believe that learning should be an exciting and engaging experience. Our methodology combines theoretical knowledge with practical application, ensuring that students not only understand the concepts but also know how to apply them in real-world scenarios." link="/methodology" />
-        <SectionCard title="Subjects We Offer" text="We offer a wide range of subjects tailored to prepare students for civil service exams. From Public Administration to Law and Ethics, our curriculum is designed to cover all essential topics comprehensively." link="/subjects" />
-        <SectionCard title="Civil Service Exams" text="Preparing for civil service exams can be challenging, but with Screamford Academy, you're in good hands. Our expert instructors and comprehensive study materials will guide you every step of the way." link="/civilserviceexams" />
-        <SectionCard title="Get in Touch" text="Have questions or need more information? Our friendly support team is here to help. Contact us today to learn more about our programs and how we can assist you in achieving your career goals." link="/contact" />
+    <div className="card-group m-4">
+        <SectionCard imageSource={methodologyImage} imageDescription="Poisoned apple" title="Our Methodology" text="At Screamford Academy, we believe that learning should be an exciting and engaging experience. Our methodology combines theoretical knowledge with practical application, ensuring that students not only understand the concepts but also know how to apply them in real-world scenarios." link="/methodology" />
+        <SectionCard imageSource={subjectImage} imageDescription="Hacker" title="Subjects We Offer" text="We offer a wide range of subjects tailored to prepare students for civil service exams. From Public Administration to Law and Ethics, our curriculum is designed to cover all essential topics comprehensively." link="/subjects" />
+        <SectionCard imageSource={examsImage} imageDescription="Haunted house ghost" title="Civil Service Exams" text="Preparing for civil service exams can be challenging, but with Screamford Academy, you're in good hands. Our expert instructors and comprehensive study materials will guide you every step of the way." link="/civilserviceexams" />
+        <SectionCard imageSource={contactImage} imageDescription="Raven" title="Get in Touch" text="Have questions or need more information? Our friendly support team is here to help. Contact us today to learn more about our programs and how we can assist you in achieving your career goals." link="/contact" />
     </div>
     );
 }
