c17fcba2d10a4aeabc7dbdfc29d12e9e4e2eeed9
MartaLB95 <martalaveda@hotmail.com>
2026-01-11 10:42:56 +0100

    Hero working, missinig style details

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import Header from './components/Header.js';
-import Footer from './components/Footer.js';
-import Home from './pages/Home.js';
+import "./App.css";
+import Header from "./components/Header.js";
+import Footer from "./components/Footer.js";
+import Home from "./pages/Home.js";
 
-function App(){
+function App() {
   return (
     <div>
-      <Header /><Home /><Footer />
+      <Header />
+      <Home />
+      <Footer />
     </div>
   );
 }
-
 
 export default App;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,22 +1,26 @@
+import academyPic from '../img/academy.jpg';
+import forestPic from '../img/forest.jpg';
+import coatOfArmsPic from '../img/screamford-coat-of-arms.png';
+
 export default function Hero(){
     return(
         <div id="carouselHome" class="carousel slide">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="..."/>
+      <img src={academyPic} class="d-block w-100" alt="One of the academy buildings"/>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."/>
+      <img src={forestPic} class="d-block w-100" alt="The forest surrounding our campus in Transilvania"/>
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="..."/>
+      <img src={coatOfArmsPic} class="d-block w-100" alt="The coat of arms of Screamford Academy"/>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselHome" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselHome" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
