a423fbb17c37e84adab90c071dd4f4bc8af890ff
MartaLB95 <martalaveda@hotmail.com>
2026-03-08 10:42:45 +0100

    Database config modified for deploy

--- backend/db.js
+++ backend/db.js
@@ -1,12 +1,12 @@
 //Basic connection MySQL node.js
-const mysql = require('mysql2/promise'); // Promises for async/await
+const mysql = require('mysql2/promise');
 
 const db = mysql.createPool({
-  host: 'localhost',
-  port: 3307,
-  user: 'root',
-  password: '1234',
-  database: 'screamford_academy',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0
