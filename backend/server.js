e65b037fcf2fd786482e89e86932190301d62da6
MartaLB95 <martalaveda@hotmail.com>
2026-03-14 16:04:41 +0100

    preflight options

--- backend/server.js
+++ backend/server.js
@@ -2,14 +2,19 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+
 app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+    return res.status(200).end();
+  }
   next();
 });
+
+app.use(cors());
+app.use(express.json());
 
 const authRoutes = require("./routes/auth");
 app.use("/api", authRoutes);
