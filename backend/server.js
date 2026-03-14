e4d0249c8ce8ff61662288728d8882955bfaf234
MartaLB95 <martalaveda@hotmail.com>
2026-03-14 15:46:26 +0100

    listen to 0.0.0.0 and not only localhost

--- backend/server.js
+++ backend/server.js
@@ -10,6 +10,6 @@ app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 3001;
 
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server running on port ${PORT}`);
 });
