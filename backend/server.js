b89bdca7b67664c390a0e84c0074d45b55273537
MartaLB95 <martalaveda@hotmail.com>
2026-03-14 15:38:15 +0100

    authRoutes added

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors({
-  origin: 'https://www.screamfordacademy.online'
-}));
+app.use(cors());
 app.use(express.json());
+
+const authRoutes = require("./routes/auth");
+app.use("/api", authRoutes);
 
 const PORT = process.env.PORT || 3001;
 
