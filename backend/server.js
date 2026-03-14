b8b79606df174cfe9a07b1a628ecdf07a14e129e
MartaLB95 <martalaveda@hotmail.com>
2026-03-14 15:53:49 +0100

    headers added

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,12 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
 
 const authRoutes = require("./routes/auth");
 app.use("/api", authRoutes);
