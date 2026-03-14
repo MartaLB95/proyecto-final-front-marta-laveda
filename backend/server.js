859a5e221765adab18ba0187e180037866a66ad7
MartaLB95 <martalaveda@hotmail.com>
2026-03-14 16:30:05 +0100

    cors first

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,15 @@
-const express = require("express");
 const cors = require("cors");
 
 const app = express();
 
-app.use((req, res, next) => {
-  if (req.method === 'OPTIONS') {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    return res.status(200).end();
-  }
-  next();
-});
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'OPTIONS'],
+  allowedHeaders: ['Content-Type']
+}));
 
-app.use(cors());
+app.options('*', cors());
+
 app.use(express.json());
 
 const authRoutes = require("./routes/auth");
